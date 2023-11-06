506c8ff4148fb3c80d0682f2a8b6ebdbbe95c02e
änkitsaxenaofficial <nkitsaxenaofficial04@gmail.com>
2023-11-06 22:29:03 +0530

    Minor changes in reporter.js file

--- reporter.js
+++ reporter.js
@@ -6,22 +6,20 @@ report.generate({
 	metadata:{
         browser: {
             name: 'chrome',
-            version: '60'
+            version: '119'
         },
         device: 'Local test machine',
         platform: {
-            name: 'ubuntu',
-            version: '16.04'
+            name: 'Windows',
+            version: '11'
         }
     },
     customData: {
         title: 'Run info',
         data: [
-            {label: 'Project', value: 'Custom project'},
-            {label: 'Release', value: '1.2.3'},
-            {label: 'Cycle', value: 'B11221.34321'},
-            {label: 'Execution Start Time', value: 'Nov 19th 2017, 02:31 PM EST'},
-            {label: 'Execution End Time', value: 'Nov 19th 2017, 02:56 PM EST'}
+            {label: 'Project', value: 'Cypress Automation'},
+            {label: 'Release', value: '1.0'}
+            
         ]
     }
 });
